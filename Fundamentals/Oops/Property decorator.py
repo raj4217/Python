5f0c9557c92ba8e8136fb970bc6dc179e430c22f
Soumya Sen <ssen.mailbox@gmail.com>
2018-06-28 21:24:27 +0530

    addition

--- Fundamentals/Oops/Property decorator.py
+++ Fundamentals/Oops/Property decorator.py
@@ -7,15 +7,30 @@ class Employee:
 
     @property
     def email(self):
-        return '{}.{}@email.com'.format(self.first, self.last)
+        return f'{self.first.lower()}.{self.last.lower()}@email.com'
 
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    @fullname.setter
+    def fullname(self, name):
+        self.first, self.last = name.split(' ')
+
+    @fullname.deleter
+    def fullname(self):
+        print('Name deleted!')
+        self.first = None
+        self.last = None
+
 
 emp_1 = Employee('John', 'Smith')
+
+emp_1.fullname = 'Raj Sen'
+
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+
+del emp_1.fullname
